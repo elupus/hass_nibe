7d14267a37feb9d7e7fe2c0a4e87718de2494935
Joakim Plate <elupus@ecce.se>
2017-06-09 22:36:32 +0200

    switch to requests-authlib with automatic refresh token handling

--- nibe.py
+++ nibe.py
@@ -6,6 +6,8 @@ import logging
 import time
 import json
 import requests
+import sys
+import pickle
 
 from homeassistant.helpers.entity import Entity
 from homeassistant.components.configurator import (request_config, notify_errors, request_done)
@@ -13,9 +15,8 @@ from homeassistant.components.configurator import (request_config, notify_errors
 from homeassistant.const import (
     CONF_ACCESS_TOKEN, CONF_EMAIL, CONF_PASSWORD,
     EVENT_HOMEASSISTANT_START, EVENT_HOMEASSISTANT_STOP)
-from oauth2client.client import (OAuth2WebServerFlow, FlowExchangeError)
-from oauth2client.file import Storage
 
+from requests_oauthlib import OAuth2Session
 
 
 _LOGGER = logging.getLogger(__name__)
@@ -24,13 +25,17 @@ CHANNELS = []
 
 DOMAIN = 'nibe'
 
-REQUIREMENTS = ['oauth2client']
+REQUIREMENTS = ['requests', 'requests_oauthlib']
 
 CONF_CLIENT_ID     = 'client_id'
 CONF_CLIENT_SECRET = 'client_secret'
 CONF_REDIRECT_URI  = 'redirect_uri'
 
 BASE       = 'https://api.nibeuplink.com'
+SCOPE      = [ 'READSYSTEM' ]
+
+TOKEN_URL  = '%s/oauth/token' % BASE
+AUTH_URL   = '%s/oauth/authorize' % BASE
 
 
 def setup(hass, config):
@@ -45,41 +50,50 @@ class NibeUplink(object):
     def __init__(self, hass, config):
         """Initialize the system."""
         self.hass       = hass
-        self.config     = None
-
-        storage    = Storage(hass.config.path('nibe.token'))
-
-        self.credential = storage.get()
+        self.store      = hass.config.path('nibe.pickle')
+        self.credential = None
         self.systems    = None
 
-        if self.credential == None:
-            flow = OAuth2WebServerFlow(client_id     = config[DOMAIN].get(CONF_CLIENT_ID),
-                                       client_secret = config[DOMAIN].get(CONF_CLIENT_SECRET),
-                                       scope         = 'READSYSTEM',
-                                       redirect_uri  = config[DOMAIN].get(CONF_REDIRECT_URI),                                             state         = 'STATE',
-                                       auth_uri      = '%s/oauth/authorize' % BASE,
-                                       token_uri     = '%s/oauth/token'     % BASE)
+        token = self.token_read()
 
-            auth_uri = flow.step1_get_authorize_url()
+        self.session = OAuth2Session(
+                client_id        = config[DOMAIN].get(CONF_CLIENT_ID),
+                redirect_uri     = config[DOMAIN].get(CONF_REDIRECT_URI),
+                auto_refresh_url = TOKEN_URL,
+                scope            = SCOPE,
+                token            = token,
+                token_updater    = self.token_write
+        )
 
-            config = None
+        if not self.session.authorized:
+            auth_uri, state = self.session.authorization_url(AUTH_URL)
+
+            config_request = None
 
             def config_callback(data):
-                _LOGGER.info(data)
                 try:
-                    self.credential = flow.step2_exchange(data['code'])
-                    storage.put(self.credential)
-                    request_done(config)
-                    hass.add_job(self.update)
-                except FlowExchangeError as error:
-                    notify_errors(config, "An error occured: %s" % error)
+                    token = self.session.fetch_token(
+                                TOKEN_URL,
+                                client_secret          = config[DOMAIN].get(CONF_CLIENT_SECRET),
+                                authorization_response = data['url']
+                    )
 
-            config = request_config(hass, "Nibe Uplink Code",
+                    request_done(config_request)
+                except:
+                    notify_errors(config_request, "An error occured: %s" % sys.exc_info()[0])
+                    return
+
+                self.token_write(token)
+                hass.add_job(self.update)
+
+
+
+            config_request = request_config(hass, "Nibe Uplink Code",
                             callback    = config_callback,
-                            description = "Navigate to provided authorization link, this will redirect you to your configured redirect url. This must match what was setup in Nibe Uplink. Enter the [code] provided to that url here.",
+                            description = "Navigate to provided authorization link, this will redirect you to your configured redirect url. This must match what was setup in Nibe Uplink. Enter the complete url you where redirected too here.",
                             link_name   = "Authorize",
                             link_url    = auth_uri,
-                            fields      = [{'id': 'code', 'name': 'Code', 'type': ''}]
+                            fields      = [{'id': 'url', 'name': 'Full url', 'type': ''}]
                         )
         else:
             hass.add_job(self.update)
@@ -91,12 +105,24 @@ class NibeUplink(object):
             _LOGGER.info(self.systems)
 
     def get(self, uri, params = {}):
-        if self.credential == None:
+        if not self.session.authorized:
             return None
 
         headers = {}
-        self.credential.apply(headers)
         url = '%s/api/v1/%s' % (BASE, uri)
-        _LOGGER.info(url)
-        return requests.get(url, params=params, headers=headers).json()
+        return self.session.get(url, params=params, headers=headers).json()
+
+    def token_read(self):
+        try:
+            with open(self.store, 'rb') as myfile:
+                return pickle.load(myfile)
+        except FileNotFoundError:
+            return None
+        except:
+            _LOGGER.warning('Failed to load previous token: %s' % sys.exc_info()[0])
+            return None
+
+    def token_write(self, token):
+        with open(self.store, 'wb') as myfile:
+            pickle.dump(token, myfile)
 
