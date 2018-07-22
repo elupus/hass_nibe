1c7c18b213034dd9d3f0eb11669c555e07a7020b
Joakim Plate <joakim.plate@gmail.com>
2018-07-22 23:22:05 +0200

    Reduce PEP8 warnings

--- nibe/auth.py
+++ nibe/auth.py
@@ -11,11 +11,30 @@ from homeassistant.const import (
 
 _LOGGER = logging.getLogger(__name__)
 
+CONFIG_NAME = "Nibe Uplink authorization required"
+CONFIG_CAPTION = 'Manually set url'
+CONFIG_DESCRIPTION = """
+Please authorize Home Assistant to access nibe
+uplink by following the authorization link.
+
+Automatic configuration will only work if your
+configured redirect url is set to a url that will
+match the access url of Home Assistant. If for example
+you access Home Assisstant on http://localhost:8123
+you should set your callback url, both on Nibe Uplink
+and in Home Assistant configuration, to
+http://localhost:8123/api/nibe/auth.
+
+If automatic configuration of home assistant fails,
+you can enter the url to the webpage you get redirected
+to in the below prompt.
+"""
+
 
 class NibeAuthView(HomeAssistantView):
     """Handle nibe  authentication callbacks."""
 
-    url  = '/api/nibe/auth'
+    url = '/api/nibe/auth'
     name = 'api:nibe:auth'
 
     requires_auth = False
@@ -23,35 +42,24 @@ class NibeAuthView(HomeAssistantView):
     def __init__(self, hass, uplink, config, setup) -> None:
         """Initialize instance of the view."""
         super().__init__()
-        self.hass           = hass
-        self.uplink         = uplink
-        self.config         = config
-        self.request_id     = None
-        self.setup          = setup
+        self.hass = hass
+        self.uplink = uplink
+        self.config = config
+        self.request_id = None
+        self.setup = setup
+        self.configurator = hass.components.configurator
 
     def async_request_config(self):
-        auth_uri    = self.uplink.get_authorize_url()
+        auth_uri = self.uplink.get_authorize_url()
 
-        description = """
-Please authorize Home Assistant to access nibe uplink by following the authorization link.
-
-Automatic configuration will only work if your configured redirect url is set to
-a url that will match the access url of Home Assistant. If for example you access
-Home Assisant on http://localhost:8123 you should set your callback url, both on Nibe
-Uplink and in Home Assistant configuration, to http://localhost:8123/api/nibe/auth.
-
-If automatic configuration of home assistant fails, you can enter the url to the webpage you
-get redirected to in the below prompt.
-"""
-
-        self.request_id = self.hass.components.configurator.async_request_config(
-            "Nibe Uplink authorization required",
-            callback    = self.callback,
-            description = description,
-            link_name   = "Authorize",
-            link_url    = auth_uri,
-            fields      = [{'id': 'url', 'name': 'Full url', 'type': ''}],
-            submit_caption = 'Manually set url'
+        self.request_id = self.configurator.async_request_config(
+            CONFIG_CAPTION,
+            callback=self.callback,
+            description=CONFIG_DESCRIPTION,
+            link_name="Authorize",
+            link_url=auth_uri,
+            fields=[{'id': 'url', 'name': 'Full url', 'type': ''}],
+            submit_caption=CONFIG_CAPTION
         )
 
     async def configure(self, url):
@@ -64,13 +72,16 @@ get redirected to in the below prompt.
 
             await self.uplink.get_access_token(code)
 
-            self.hass.components.configurator.async_request_done(self.request_id)
-            self.hass.async_add_job(self.setup(self.hass, self.config, self.uplink))
+            self.configurator.async_request_done(self.request_id)
+            self.hass.async_add_job(
+                self.setup(self.hass, self.config, self.uplink)
+            )
 
         except BaseException:
-            self.hass.components.configurator.async_notify_errors(
+            self.configurator.async_notify_errors(
                 self.request_id,
-                "An error occured during nibe authorization. See logfile for more information."
+                """An error occured during nibe authorization.
+                   See logfile for more information."""
             )
             raise
 
@@ -85,8 +96,9 @@ get redirected to in the below prompt.
         except BaseException:
             msg = "An error occured during nibe authorization."
             _LOGGER.exception(msg)
-            return self.json_message(msg, status_code =HTTP_BAD_REQUEST)
+            return self.json_message(msg, status_code=HTTP_BAD_REQUEST)
         else:
-            msg = "Nibe has been authorized! you can close this window, and restart Home Assistant."
+            msg = """Nibe has been authorized!
+                     you can close this window, and restart Home Assistant."""
             _LOGGER.info(msg)
-            return self.json_message(msg, status_code =HTTP_OK)
+            return self.json_message(msg, status_code=HTTP_OK)
